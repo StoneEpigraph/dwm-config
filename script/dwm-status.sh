9d4e89ccba6ab98b8043be928fc00e3ab14c9947
StoneEpigraph <stoneepigraph@163.com>
2021-11-15 22:37:37 +0800

    config systray using emjio

--- script/dwm-status.sh
+++ script/dwm-status.sh
@@ -4,20 +4,23 @@
 Battery=$(cat /sys/class/power_supply/BAT1/capacity)%
 BatteryStatus=$(cat /sys/class/power_supply/BAT1/status)
 if [[ $BatteryStatus == D* ]]; then
-    BatteryStatus='NoCharge'
+    BatteryStatus='🔋'
 else
-    BatteryStatus='Charging'
+    BatteryStatus='🔌'
 fi
 
 #volume
+VolumeStatus=''
 Volume=$(amixer get Master | tail -n1 | cut -d '[' -f4 | cut -d ']' -f1)
 if [[ $Volume == off ]]; then
+    Volume='🔇'
     Volume='mute'
 else 
+    VolumeStatus='🔊'
     Volume=$(amixer get Master | sed -n '5p' | cut -d '[' -f2 | cut -d% -f1)
 fi
 
 #datetime
 DateTime=$(date +"%a %m-%d %T")
 
-xsetroot -name "Vol.$Volume|$DateTime|[$BatteryStatus:$Battery]"
+xsetroot -name " $VolumeStatus$Volume% ⏰$DateTime $BatteryStatus:$Battery"
